2074d0e84bf608030e778d41ec4ff3ab956345f6
Den4200 <dpham.42@hotmail.com>
2020-04-25 16:22:50 -0400

    fix map classmethod

--- monkeys-and-frogs-on-fire/triple_vision/map.py
+++ monkeys-and-frogs-on-fire/triple_vision/map.py
@@ -18,28 +18,29 @@ class Map:
     FLOOR = 2
     SPIKE = 3
 
+    GENERATIONS = 6
+    FILL_PROBABILITY = 0.2
+    SPIKE_PROBABILITY = 0.01
+
     def __init__(self, view: arcade.View, shape: Tuple[int, int]) -> None:
         self.view = view
         self.shape = shape
 
-        self.GENERATIONS = 6
-        self.FILL_PROBABILITY = 0.2
-        self.SPIKE_PROBABILITY = 0.01
-
         self.sprites: Optional[arcade.SpriteList] = None
 
-    def generate(self) -> np.array:
-        map_ = np.ones(self.shape)
+    @classmethod
+    def generate(cls, shape) -> np.array:
+        map_ = np.ones(shape)
 
-        for i in range(self.shape[0]):
-            for j in range(self.shape[1]):
+        for i in range(shape[0]):
+            for j in range(shape[1]):
                 choice = random.uniform(0, 1)
-                map_[i][j] = self.WALL \
-                    if choice < self.FILL_PROBABILITY else self.FLOOR
+                map_[i][j] = cls.WALL \
+                    if choice < cls.FILL_PROBABILITY else cls.FLOOR
 
-        for gen in range(self.GENERATIONS):
-            for i in range(self.shape[0]):
-                for j in range(self.shape[1]):
+        for gen in range(cls.GENERATIONS):
+            for i in range(shape[0]):
+                for j in range(shape[1]):
 
                     # Get walls that are 1 away from each index
                     submap = map_[
@@ -47,37 +48,37 @@ class Map:
                         max(j - 1, 0): min(j + 2, map_.shape[1])
                     ]
                     flat_submap = submap.flatten()
-                    wall_count_1_away = len(np.where(flat_submap == self.WALL)[0])
-                    floor_count_1_away = len(np.where((flat_submap == self.FLOOR) | (flat_submap == self.SPIKE))[0])
+                    wall_count_1_away = len(np.where(flat_submap == cls.WALL)[0])
+                    floor_count_1_away = len(np.where((flat_submap == cls.FLOOR) | (flat_submap == cls.SPIKE))[0])
 
                     # Get walls that are 2 away from each index
                     submap = map_[
                         max(i - 2, 0): min(i + 3, map_.shape[0]),
                         max(j - 2, 0): min(j + 3, map_.shape[1])
                     ]
-                    wall_count_2_away = len(np.where(submap.flatten() == self.WALL)[0])
+                    wall_count_2_away = len(np.where(submap.flatten() == cls.WALL)[0])
 
                     # First (self.GENERATIONS - 1) generations build scaffolding for walls
-                    if gen < self.GENERATIONS - 1:
+                    if gen < cls.GENERATIONS - 1:
 
                         # If 1 away has 5 or more walls, make the current point a wall
                         # If 2 away has 7 or more walls, make the current point a wall
                         # If neither, make the current point floor
                         if wall_count_1_away >= 5 or wall_count_2_away <= 7:
-                            map_[i][j] = self.WALL
+                            map_[i][j] = cls.WALL
                         else:
                             chance = random.uniform(0, 1)
-                            map_[i][j] = self.FLOOR \
-                                if chance > self.SPIKE_PROBABILITY else self.SPIKE
+                            map_[i][j] = cls.FLOOR \
+                                if chance > cls.SPIKE_PROBABILITY else cls.SPIKE
 
                         # Make the current point a wall if it's on the edge of the map
-                        if i == 0 or j == 0 or i == self.shape[0] - 1 or j == self.shape[1] - 1:
-                            map_[i][j] = self.WALL
+                        if i == 0 or j == 0 or i == shape[0] - 1 or j == shape[1] - 1:
+                            map_[i][j] = cls.WALL
 
                     else:
                         # Turn all walls that aren't near floor into air
-                        if floor_count_1_away == 0 and map_[i][j] == self.WALL:
-                            map_[i][j] = self.AIR
+                        if floor_count_1_away == 0 and map_[i][j] == cls.WALL:
+                            map_[i][j] = cls.AIR
 
         return map_
 
@@ -131,7 +132,7 @@ class Map:
         map_ = None
 
         while floor_count < (shape[0] * shape[1]) / 2.5:
-            map_ = cls.generate()
+            map_ = cls.generate(shape)
             flat_map = map_.flatten()
             floor_count = len(np.where((flat_map == cls.FLOOR) | (flat_map == cls.SPIKE))[0])
 
